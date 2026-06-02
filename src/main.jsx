7050a8eb3c2f65c91dfb0b467af1bb6ff53f7b57
Nazrul Islam <dobon.islam@icloud.com>
2026-06-02 09:59:41 -0400

    theme added from daisyUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import RootLayout from './layout/RootLayout/RootLayout.jsx';
 import Home from './pages/Home/Home.jsx';
 import AllProducts from './AllProducts/AllProducts.jsx';
+import AuthProvider from './context/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />,
+    <AuthProvider>
+       <RouterProvider router={router} />,
+    </AuthProvider>
   </StrictMode>,
 )
